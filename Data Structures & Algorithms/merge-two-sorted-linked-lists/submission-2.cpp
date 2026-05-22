b338d630b80579bbe8615ad04e811b51a0a06277
Mogullapalli-Akshita <140147991+Mogullapalli-Akshita@users.noreply.github.com>
2026-05-22 12:05:26 +0530

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // Dummy node to simplify head tracking
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        if (list1) curr->next = list1;
+        if (list2) curr->next = list2;
+        return dummy->next;
+    }
+};
